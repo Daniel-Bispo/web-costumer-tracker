2bec6d13fd1bf63ed854cb5c798c1f50d0b389ee
Daniel Bispo <danielvbispo@outlook.com>
2019-02-19 12:30:05 -0300

    Implementacao de CustomerDAO e CustomerDAOImpl

---
+++ src/com/webcostumertracker/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.webcostumertracker.dao;
+
+import java.util.List;
+
+import com.webcostumertracker.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
